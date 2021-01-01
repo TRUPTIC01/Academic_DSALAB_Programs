ddfbbbcd552f800a6ac932078cdc89eabe557eec
TRUPTIC01 <truptichinchole1@gmail.com>
2021-01-01 16:04:26 +0530

    structure

---
+++ struct.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+struct student {
+
+	char name [30];
+	int roll_no;
+	float marks;
+
+};
+
+int main() { 
+
+	struct student s;
+
+	printf("Enter Student information\n");
+	printf("Enter name\n");
+	scanf("%s",s.name);
+	printf("Enter roll_no\n");
+	scanf("%d",&s.roll_no);
+	printf("Enter marks\n");
+	scanf("%f",&s.marks);
+
+	printf("Displaying information\n");
+	printf("%s\n",s.name);
+	printf("%d\n",s.roll_no);
+	printf("%f\n",s.marks);
+
+	return 0;
+
+}
